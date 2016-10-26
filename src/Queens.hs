a53fa75fa17d2bb1de826564929e1153fb46bc73
MZober1993 <Matthias.Zober1993@outlook.de>
2016-10-26 23:44:03 +0200

    simplify something

--- src/Queens.hs
+++ src/Queens.hs
@@ -1,13 +1,15 @@
 import Ersatz
 import Prelude hiding ((&&),(||),not,or)
 import qualified Prelude
-import Control.Monad (forM, when)
+import Control.Monad (forM, when,unless)
+import Control.Applicative
 import System.Environment (getArgs)
 
 main = do
-    argv <- getArgs
+    queens 8
+   {- argv <- getArgs
     case argv of
-        [s] -> queens (read s)
+        [s] -> queens (read s)-}
 
 queens :: Int -> IO ()
 queens n = do
@@ -20,6 +22,8 @@ queens n = do
     -- p :: (Int,Int) -> Bit
     forM positions $ \ a -> forM positions $ \ b ->
       when (attacks a b) $ assert $ not (p a) || not (p b)
+    {-forM positions $ \ a -> forM positions $ \ b ->
+      unless (attacks a b) $ assert $ (p a) && (p b)-}
     return (xs :: [[Bit]])
   print (res, bel :: Maybe [[Bool]])
 -- still unsat
@@ -27,9 +31,14 @@ attacks (z1,s1)(z2,s2) = (z1 == z2 ) Prelude.|| (s1 == s2) Prelude.|| (z1 - s1 =
 
 queen_pos = [(0,0),(1,6),(2,4),(3,7),(4,1),(5,3),(6,5),(7,2)]
 
-cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+-- equals to:     > [(x,y) | x <- xs, y <- ys]
+-- also equals to:> liftM2 (,) xs xy
+                  --cartProd :: [a] -> [b] -> [(a, b)]
+--                > cartProd xs ys = (,) <$> xs <*> ys
+selfCartProd :: [a] -> [(a,a)]
+selfCartProd xs = (,) <$> xs <*> xs
 
 test :: Bool -- True = all queen_pos will fail attacks == the wanted behaviour
 test = Prelude.all (\ (x,y,b) -> Prelude.not b ) q_tuples
 
-q_tuples = map (\ (x,y) -> (x,y,attacks x y )) $ filter (\ (x,y) -> x /= y) $ cartProd queen_pos queen_pos
+q_tuples = map (\ (x,y) -> (x,y,attacks x y )) $ filter (\ (x,y) -> x /= y) $ selfCartProd queen_pos
